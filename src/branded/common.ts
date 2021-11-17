b50f144f61c664669a5044b588b030cdcc73af44
shrooms <shrooms@deeplay.io>
2021-11-18 00:38:32 +0400

    feat: added branded common and tsonfig fixed

---
+++ src/branded/common.ts
@@ -0,0 +1,35 @@
+import { BrandedPrimitive } from "./index";
+
+export const StringX = {
+  ofString: (
+    rules: {
+      min?: number;
+      max?: number;
+    },
+    value: string
+  ) => {
+    if (rules.min !== undefined && value.length < rules.min) {
+      return new Error(
+        `${value} must be minimum ${rules.min} symbols`
+      );
+    }
+
+    if (rules.max !== undefined && value.length > rules.max) {
+      return new Error(
+        `${value} must be maximum ${rules.max} symbols`
+      );
+    }
+
+    return value;
+  },
+};
+
+export type NotEmptyString = BrandedPrimitive<
+  string,
+  { readonly NotEmptyString: unique symbol }
+>;
+export const NotEmptyString = {
+  ofString: (value: string): NotEmptyString => {
+    return StringX.ofString({ min: 1 }, value) as NotEmptyString;
+  },
+};
--- src/branded/index.ts
+++ src/branded/index.ts
@@ -1,3 +1,5 @@
+export * from "./common"
+
 export type Branded<T, Brand> = T & Brand
 type Primitive = string | number | boolean | Date | string[] | number[] | boolean[] | Date[] | null | Buffer
 export type BrandedPrimitive<Type extends Primitive, Brand> = Branded<Type, Brand>
