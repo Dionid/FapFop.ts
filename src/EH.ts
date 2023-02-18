2eca34b8322ccc0a84c51bda748f3f900cb15859
David Shek <ditreyw@gmail.com>
2023-02-18 13:36:38 +0000

    feat: added error handler

---
+++ src/EH.ts
@@ -0,0 +1,44 @@
+
+
+export const returnOnThrow = <Args extends any[], R>(fn: (...args: Args) => R) => {
+    return async (...args: Args): Promise<R | Error> => {
+        try {
+            return await fn(...args)
+        } catch (err) {
+            if (err instanceof Error) {
+                return err
+            }
+            throw err
+        }
+    }
+}
+
+export const throwOnError = <Args extends any[], R>(fn: (...args: Args) => R) => {
+    return async (...args: Args): Promise<R> => {
+        const res = await fn(...args)
+
+        if (res instanceof Error) {
+            throw res
+        }
+
+        return res
+    }
+}
+
+export const throwOnUndefined = <Args extends any[], R>(fn: (...args: Args) => R | undefined, error: Error = new Error(`Result is undefined`)) => {
+    return async (...args: Args): Promise<R> => {
+        const res = await fn(...args)
+
+        if (res === undefined) {
+            throw error
+        }
+
+        return res
+    }
+}
+
+
+export const EH = {
+    returnOnThrow,
+    throwOnError,
+}
